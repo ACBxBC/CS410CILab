85f238fb7cc194e08ef51594d414e16cc5a82f4f
ACBxBC <a.bautista@bellevuecollege.edu>
2023-06-09 10:40:58 -0700

    replace override method try

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -13,7 +13,30 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
-        return false;
+        if (myString == null || myString.isEmpty()) {
+            return true;
+        }
+
+        int numOfCapitalLetters = 0;
+
+        for (char c : myString.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                numOfCapitalLetters++;
+            }
+        }
+
+        // All letters are uppercase
+        if (numOfCapitalLetters == myString.length()) {
+            return true;
+        }
+
+        // All letters are lowercase
+        if (numOfCapitalLetters == 0) {
+            return true;
+        }
+
+        // Only the first letter is uppercase
+        return numOfCapitalLetters == 1 && Character.isUpperCase(myString.charAt(0));
     }
 
 }
